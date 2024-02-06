f6847ca746de41f30c639c55cda7208ca7cf9892
Wollfiksk <ondracekj23@spstrutnov.cz>
2024-02-06 18:26:14 +0100

    funguje vzlet

--- Source/main.py
+++ Source/main.py
@@ -1,7 +1,6 @@
 import pygame
 import sys
 import random
-from player import player
 
 pygame.init()
 pygame.font.init()
@@ -9,41 +8,13 @@ pygame.font.init()
 rozliseni_okna = (800, 600)
 
 okno = pygame.display.set_mode(rozliseni_okna)
-soubor = open("G:\Python\projecterectermecternerder\Mine-nuhazgvdtzasfztHSFDztjhdzjgHASYGtfrSZTJ-HGJfSAUTFDuzr-IKSDJ-H\safe\Save.txt", 'r', encoding = 'utf-8')
-
-for jeden_radek in soubor:
-    skorestr = jeden_radek
-    skore = int(skorestr)
-        
-
-soubor.close()
-
-randoms = random.randint(50,500)
-pozice_micku_y = randoms
-pozice_micku_x = 400
-velikost_micku = 50
-randoms2 = random.randint(1,2)
-if randoms2 == 1:
-    rychlost_micku_x = 0.3
-if randoms2 == 2:
-    rychlost_micku_x = -0.3
-rychlost_micku_y = 0.3
-
-velikost_hrace_vyska = 150
-velikost_hrace_sirka = 50
-
-pozice_hrace_x = 10
-pozice_hrace_y = 300
-rychlost_hrace = 0.6
-
-velikost_hrace_vyska2 = 150
-velikost_hrace_sirka2 = 50
 
 clock = pygame.time.Clock()
 
-pozice_hrace_x2 = 740
-pozice_hrace_y2 = 300
-rychlost_hrace2 = 0.6
+playerx = 20
+playery = 440
+velikostx = 40
+velikosty = 50
 
 button_rect2 = pygame.Rect(300, 200, 200, 100)
 button_rect = pygame.Rect(300, 330, 200, 100)
@@ -115,9 +86,6 @@ while True:
         if is_hovered2:
             if pygame.mouse.get_pressed()[0]:  # Left mouse button is pressed
                 draw_button(button_rect2, click_color, "Quited and Saved")
-                soubor2 = open("G:\Python\projecterectermecternerder\Mine-nuhazgvdtzasfztHSFDztjhdzjgHASYGtfrSZTJ-HGJfSAUTFDuzr-IKSDJ-H\safe\Save.txt", 'w', encoding = 'utf-8')
-                soubor2.write(str(skore))
-                soubor2.close()
                 
                 quit()
             else:
@@ -127,43 +95,21 @@ while True:
             
         pygame.display.flip()
         
-    playerx = 20
-    playery = 440
-    velikostx = 40
-    velikosty = 60
     
     if not esc:            
         stisknute_klavesy = pygame.key.get_pressed()
         okno.fill((0, 0, 0))
         
+        direction = pygame.Vector2(playerx, playery)
+        pygame.draw.rect(okno, (255, 255, 255), (playerx, playery, velikostx, velikosty))
+
+
         fps_counter()
-        clock.tick(60)  
-        
+        clock.tick(100)
+                
         #jump
         if stisknute_klavesy[pygame.K_SPACE]:
-            on_ground = playery == 440
-            jump_count = 10  
-
-            if on_ground:
-                jump_count = 10  
-
-            if stisknute_klavesy[pygame.K_SPACE]:
-                if on_ground:
-                    jump_count = 10  
-            else:
-                jump_count = 0  
-
-            if not on_ground or jump_count > 0:
-                playery -= 3
-                jump_count -= 1
-
-            if playery < 440:
-                on_ground = False
-            else:
-                on_ground = True
-                playery =  440
-
-        player(20, 440, 40, 60)
+            playery -= 1
      
         pygame.display.update()
     
